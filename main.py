7d2aaddb552cf8f7419de707faf6c1b3f04dacf3
px1208 <px12086@gmail.com>
2025-04-17 14:56:47 -0400

    4.17
    4.17

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ class bcolors:
 def misty_forest():
     player_position = 0
     zombie_position = -10
-    goal = 30
-    oldman_position = random.randint(1, 30)
-    compass_position = random.randint(1, 15)
-    pitfall_position = random.randint(1, 15)
-    windstorm_position = random.randint(1, 15)
-    compass_position2 = random.randint(16, 30)
-    pitfall_position2 = random.randint(16, 30)
-    windstorm_position2 = random.randint(16, 30)
+    goal = 35
+    oldman_position = random.randint(1, 35)
+    compass_position = random.randint(1, 17)
+    pitfall_position = random.randint(1, 17)
+    windstorm_position = random.randint(1, 17)
+    compass_position2 = random.randint(18, 35)
+    pitfall_position2 = random.randint(18, 35)
+    windstorm_position2 = random.randint(18, 35)
 
     oldman_used = False
 
     print(bcolors.PURPLE + "Escape from the Misty Forest!" + bcolors.ENDC)
-    print(bcolors.PURPLE + "You will: avoid zombies and reach the destination" + bcolors.ENDC)
+    print(bcolors.PURPLE + "You will: avoid zombies and reach the destination(35)" + bcolors.ENDC)
     print(bcolors.PURPLE + "There is a god who can help you. In each round, the god will choose a number from 123. If you have telepathy with her, she can let you move forward." + bcolors.ENDC)
     print("=" * 60)
  
     while player_position < goal:
-          computer_choice = random.choice("1,2,3")
+          computer_choice = random.choice(["1", "2", "3"])
 
           if player_position == oldman_position and not oldman_used:
                print("🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️🧙‍♂️ You meet an oldman, he told you that the number God would choose for the next round is", computer_choice)
@@ -65,10 +65,10 @@ def misty_forest():
                player_position += storm_effect
                print("Your position: " + bcolors.YELLOW + f"{player_position}" + bcolors.ENDC)
                print("~" * 60)
-          
+
           guess = input("What number did you guess?")
-          move = random.choice([1,2,3,4,5])
-     
+          move = random.choice([1, 2, 3, 4, 5])
+
           if guess == computer_choice:
                print(bcolors.GREEN + f"Guess it! You walked safely for {move} steps." + bcolors.ENDC)
                player_position += move
